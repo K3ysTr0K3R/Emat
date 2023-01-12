75df86d79083864024d03265716d8c131a182460
k3ystr0k3r <70909693+K3ysTr0K3R@users.noreply.github.com>
2023-01-12 01:51:47 -0700

    Update Emat.py

--- Emat.py
+++ Emat.py
@@ -1,13 +1,62 @@
 #!/bin/python3
 
+import os
 import shlex
 import subprocess
+from colorama import Fore as color
+
+RED = color.RED
+BLUE = color.BLUE
+YELLOW = color.YELLOW
+CYAN = color.CYAN
+GREEN = color.GREEN
+
+os.system('clear')
+print(f"{BLUE}")
+print("########################################")
+print("# Coded By: K3ysTr0K3R                 #")
+print("# Email crawling worm that scrapes     #")
+print("# for emails on your current directory #")
+print("########################################")
+print(f"{GREEN}")
+print("             uuuuuuu")
+print("         uuuuuuuuuuuuuuu")
+print("      uuuuuuuuuuuuuuuuuuuuu")
+print("     uuuuuuuuuuuuuuuuuuuuuuu")
+print("    uuuuuuuuuuuuuuuuuuuuuuuuu")
+print("   uuuuuuuuuuuuuuuuuuuuuuuuuuu")
+print("   uuuuuuuuuuuuuuuuuuuuuuuuuuu")
+print("   uuuuuuu     uuu     uuuuuuu")
+print("    uuuu       uuu       uuuu")
+print("    uuuu       uuu       uuuu")
+print("     uuuu     uuuuu      uuuu")
+print("      uuuuuuuuu   uuuuuuuuu")
+print("       uuuuuuu     uuuuuuu")
+print("        uuuuuuuuuuuuuuuuu")
+print("         uu u u u u u uu")
+print("         uuuu u u u uuuu")
+print("          uuuuuuuuuuuuu")
+print("            uuuuuuuuu")
+print("")
+print("@@@@@@@")
+print("@        @    @    @@     @@@@@")
+print("@        @@  @@   @  @      @")
+print("@@@@@    @ @@ @  @    @     @")
+print("@        @    @  @@@@@@     @")
+print("@        @    @  @    @     @")
+print("@@@@@@@  @    @  @    @     @")
+print("")
+print(f"{RED}<--------------------------------------------------------->")
+print(f"{YELLOW}[{GREEN}!!!{YELLOW}] {GREEN}Your security makes me wanna go LOLOLOLOLOLOLOLOL")
+print(f"{RED}<--------------------------------------------------------->")
+print("")
 
 current_directory = subprocess.check_output(['pwd']).strip().decode('utf-8')
-print(f'[!] Crawling Your Current Directory: {current_directory}')
+print(f'{YELLOW}[{GREEN}!{YELLOW}] {GREEN}Crawling Your Current Directory: {RED}{current_directory}')
 
 emails = subprocess.check_output(['grep', '-hrioP', r'\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+\b'])
 emails = emails.strip().decode('utf-8')
+
 emails = emails.split('\n')
 
 gmail_emails = [email for email in emails if 'gmail' in email]
@@ -21,45 +70,41 @@ hotmail_encoded = '\n'.join(hotmail_emails).encode('utf-8').decode('utf-8')
 government_encoded = '\n'.join(government_emails).encode('utf-8').decode('utf-8')
 
 if not gmail_emails:
-	print("")
-	print('[~] No Gmail accounts were found')
-  print("")
+	print('')
+	print(f'{YELLOW}[{RED}~{YELLOW}] {CYAN}No Gmail accounts were found ')
+	print('')
 else:
-	print("")
-	print('[+] Gmail accounts were found ')
-	print("")
+	print('')
+	print(f'{YELLOW}[{BLUE}+{YELLOW}] {CYAN}Gmail accounts were found ')
 	print('\n'.join(gmail_emails))
-	print("")
+	print('')
 
 if not yahoo_emails:
-	print("")
-	print('[~] No Yahoo accounts were found ')
-	print("")
+	print('')
+	print(f'{YELLOW}[{RED}~{YELLOW}] {CYAN}No Yahoo accounts were found')
+	print('')
 else:
-	print("")
-	print('[+] Yahoo accounts were found ')
-	print("")
+	print('')
+	print(f'{YELLOW}[{BLUE}+{YELLOW}] {CYAN}Yahoo accounts were found')
 	print('\n'.join(yahoo_emails))
-	print("")
+	print('')
 
 if not hotmail_emails:
-	print("")
-	print('[~] No Hotmail accounts were found ')
-	print("")
+	print('')
+	print(f'{YELLOW}[{RED}~{YELLOW}] {CYAN}No Hotmail accounts were found')
+	print('')
 else:
-	print("")
-	print('[+] Hotmail mail accounts were found ')
-	print("")
+	print('')
+	print(f'{YELLOW}[{BLUE}+{YELLOW}] {CYAN}Hotmail mail accounts were found')
 	print('\n'.join(hotmail_emails))
-	print("")
+	print('')
 
 if not government_emails:
-	print('<------------------------------->')
-	print('[~] No Hotmail accounts were found ')
-	print('<------------------------------->')
+	print('')
+	print(f'{YELLOW}[{RED}~{YELLOW}] {CYAN}No Government accounts were found')
+	print('')
 else:
-	print("")
-	print('[+] Hotmail mail accounts were found ')
-	print("")
+	print('')
+	print(f'{YELLOW}[{BLUE}+{YELLOW}] {CYAN}Government accounts were found')
 	print('\n'.join(government_emails))
-	print("")
+	print('')
